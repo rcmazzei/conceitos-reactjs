93c8ad7dc4f6f93f2d29ed349653451644c179a9
Renato Mazzei de Campos <rcmazzei@outlook.com>
2020-06-18 22:12:45 -0300

    Listagem, inclusão e remoção de repositórios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React from "react";
+﻿import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repository, setRepository] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [initialState, setInitialState] = useState(true);
+
+  useEffect(() => {
+
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+    
+
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    if (!repository) {
+      return;
+    }
+
+    const { data } = await api.post('/repositories', {
+      title: repository,
+      url: null,
+      techs: null
+    });
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`/repositories/${id}`);
+
+    const filteredRepositories = repositories.filter(repository => repository.id !== id);
+    setRepositories(filteredRepositories);
+    setInitialState(false);
+
+  }
+
+  async function handleRepositoryChange(event) {
+
+    setRepository(event.target.value);
+
   }
 
   return (
-    <div>
+    <div>     
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {initialState === true &&
+          <h1>Desafio ReactJS</h1>  
+        }
+        {
+          repositories.map(repository => (
+              <li key={repository.id}>
+                {repository.title}
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+            ))
+        }
       </ul>
-
+      <input type="text" placeholder="Nome do Repositório" onChange={handleRepositoryChange}></input>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
